e7e84852da97ea3a397588835d9b941746dde8b5
ReyesDeJong <reyes.dejong@gmail.com>
2019-11-18 23:26:32 -0300

    apply transformations to batch ready, no labels retrieved yet

--- modules/geometric_transform/transformations_tf.py
+++ modules/geometric_transform/transformations_tf.py
@@ -1,7 +1,6 @@
 import abc
 import itertools
 
-import numpy as np
 import tensorflow as tf
 
 
@@ -23,11 +22,13 @@ class AffineTransformation(object):
                               [[0, 0], [np.abs(self.ty), np.abs(self.ty)],
                                [np.abs(self.tx), np.abs(self.tx)],
                                [0, 0]], "REFLECT")
-        res_x_translated = res_x_padded#tf.contrib.image.translate(
-            # res_x_padded, [self.tx, self.ty])
+        res_x_translated = res_x_padded  # tf.contrib.image.translate(
+        # res_x_padded, [self.tx, self.ty])
         res_x = res_x_translated[:,
-                np.abs(self.ty)+self.ty:np.abs(self.ty)+self.ty + res_x.shape[1],
-                np.abs(self.tx)+self.tx:np.abs(self.tx)+self.tx + res_x.shape[2], :]
+                np.abs(self.ty) + self.ty:np.abs(self.ty) + self.ty +
+                                          res_x.shape[1],
+                np.abs(self.tx) + self.tx:np.abs(self.tx) + self.tx +
+                                          res_x.shape[2], :]
     if self.k_90_rotate != 0:
       with tf.name_scope("rotation"):
         res_x = tf.image.rot90(res_x, k=self.k_90_rotate)
@@ -48,6 +49,7 @@ class AbstractTransformer(abc.ABC):
   def _create_transformation_list(self):
     return
 
+  # This must be included within preprocessing mapping(?)
   def transform_batch(self, x, t_inds):
     transformed_batch = []
     with tf.name_scope("transformations"):
@@ -56,7 +58,6 @@ class AbstractTransformer(abc.ABC):
       concatenated_transformations = tf.concat(transformed_batch, axis=0)
     return tf.identity(concatenated_transformations, 'concat_transforms')
 
-  # Todo: delegate this to transformer
   def apply_all_transforms(self, x):
     """generate transform inds, that are the labels of each transform and
     its respective transformed data"""
@@ -102,6 +103,7 @@ class SimpleTransformer(AbstractTransformer):
 
     self._transformation_list = transformation_list
 
+
 def test_visualize_transforms():
   import imageio
   import glob
@@ -126,7 +128,6 @@ def test_visualize_transforms():
   transformer = Transformer(16, 16)
   transformations_inds = np.arange(transformer.n_transforms)
 
-
   transformed_batch = transformer.transform_batch(im,
                                                   transformations_inds)
 
@@ -140,12 +141,18 @@ def test_visualize_transforms():
       plt.show()
 
 
-
 if __name__ == "__main__":
   import imageio
   import glob
   import os, sys
   import matplotlib.pyplot as plt
+  import datetime
+  import time
+  import numpy as np
+
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
   PROJECT_PATH = os.path.abspath(
       os.path.join(os.path.dirname(__file__), '..', '..'))
@@ -156,28 +163,39 @@ if __name__ == "__main__":
   for im_path in glob.glob(im_path):
     im = imageio.imread(im_path)
 
-  im = im[np.newaxis, :150, :150, :]
+  im = im[np.newaxis, :63, :63, :]
   im = im / np.max(im)
   print(im.shape)
-  plt.imshow(im[0])
-  plt.show()
+  # plt.imshow(im[0])
+  # plt.show()
 
   x_test = np.repeat(im, 1000, axis=0)
   test_ds = tf.data.Dataset.from_tensor_slices((x_test)).batch(32)
-
   transformer = Transformer(16, 16)
   transformations_inds = np.arange(transformer.n_transforms)
 
+  EPOCHS = 2
 
-  transformed_batch = transformer.transform_batch(im,
-                                                  transformations_inds)
+  start_time = time.time()
+  for epoch in range(EPOCHS):
+    transformed_dataset = []
+    for images in test_ds:
+      transformed_batch = transformer.transform_batch(images,
+                                                      transformations_inds)
+      transformed_dataset.append(transformed_batch)
+      # print(transformed_batch.shape)
+    transformed_dataset = np.concatenate(
+        [tensor.numpy() for tensor in transformed_dataset])
+  print(transformed_dataset.shape)
 
-  print(transformed_batch.shape)
+  time_usage = str(datetime.timedelta(
+      seconds=int(round(time.time() - start_time))))
+  print("Time usage %s: %s" % (transformer.name, str(time_usage)), flush=True)
 
-  for i in range(72):
-    transform_indx = i
-    if (i % 4) == 0:
-      plt.imshow(transformed_batch[transform_indx])
-      plt.title(str(transformer.tranformation_to_perform[i]))
-      plt.show()
-
+  # for i in range(72):
+  #   transform_indx = i
+  #   if (i % 4) == 0:
+  #     plt.imshow(transformed_batch[transform_indx])
+  #     plt.title(str(transformer.tranformation_to_perform[i]))
+  #     plt.show()
+  #
