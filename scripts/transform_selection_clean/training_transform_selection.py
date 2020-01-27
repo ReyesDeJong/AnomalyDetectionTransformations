97dddd4a2b355d2158bb3964048d12ff25bb50d9
ReyesDeJong <reyes.dejong@gmail.com>
2020-01-26 21:14:05 -0300

    changed paths of all report ts

--- scripts/transform_selection_clean/training_transform_selection.py
+++ scripts/transform_selection_clean/training_transform_selection.py
@@ -55,14 +55,14 @@ def check_for_discrepancies_between_train_and_val_acc_matrix(
   transform_selection_results_path = os.path.join(
     TRANSFORM_SELECTION_RESULTS_FOLDER, TRANSFORM_SELECTION_RESULTS_FILENAME)
   if val_transformer.tranformation_to_perform == train_transformer.tranformation_to_perform:
-    with open(TRANSFORM_SELECTION_RESULTS_FILENAME, "a") as myfile:
+    with open(transform_selection_results_path, "a") as myfile:
       myfile.write(
           "\n" + model.name + '\n' + transformer.name + '\n' + data_loader.name)
       myfile.write(
           'Train and Val Transforms selected are equal')
   else:
     print('DISCREPANCY OF CONFLICTING TRANSFORMS VAL-TRAIN')
-    with open(TRANSFORM_SELECTION_RESULTS_FILENAME, "a") as myfile:
+    with open(transform_selection_results_path, "a") as myfile:
       myfile.write(
           "\n" + model.name + '\n' + transformer.name + '\n' + data_loader.name)
       myfile.write(
