9f90fdd55e7476bf50863c8bd811f095c9698c3c
ReyesDeJong <reyes.dejong@gmail.com>
2020-07-22 19:06:19 -0400

    pipeline object saving final transformations

--- modules/transform_selection/pipeline/pipeline_selection.py
+++ modules/transform_selection/pipeline/pipeline_selection.py
@@ -16,9 +16,10 @@ from modules.geometric_transform.transformations_tf import AbstractTransformer
 from modules.data_loaders.hits_outlier_loader import HiTSOutlierLoader
 from modules.transform_selection.pipeline.abstract_selector import \
     AbstractTransformationSelector
-from modules.utils import check_path
+from modules.utils import check_path, save_pickle
 from typing import List
 from modules.print_manager import PrintManager
+import pandas as pd
 
 
 # TODO: disentangle mess with other functions and methods, and matrix savers
@@ -41,9 +42,10 @@ class PipelineTransformationSelection(object):
         return results_folder_path
 
     def get_pipeline_name(
-        self, transformer: AbstractTransformer, data_loader: HiTSOutlierLoader):
+        self, transformer: AbstractTransformer, data_loader: HiTSOutlierLoader,
+        n_transforms_orig: int):
         pipeline_name = 'pipeline_%s_%s%i' % (
-            data_loader.name, transformer.name, transformer.n_transforms)
+            data_loader.name, transformer.name, n_transforms_orig)
         for selector in self.pipeline_transformation_selectors:
             pipeline_name += '_%s' % selector.name
         return pipeline_name
@@ -66,28 +68,52 @@ class PipelineTransformationSelection(object):
 
     def _save_selected_transformations(
         self, step: int, transformer: AbstractTransformer,
+        n_transforms_orig: int, dataset_loader: HiTSOutlierLoader):
+        pipeline_name = self.get_pipeline_name(transformer, dataset_loader,
+                                               n_transforms_orig)
+        pipeline_save_file_name = '%s_step%i.pkl' % (pipeline_name, step)
+        save_path = os.path.join(self.results_folder_path,
+                                 pipeline_save_file_name)
+        save_pickle(transformer.transformation_tuples, save_path)
+
+    def _get_pipeline_final_save_path(
+        self, transformer: AbstractTransformer, n_transforms_orig,
         dataset_loader: HiTSOutlierLoader):
-        pipeline_name = self.get_pipeline_name(transformer, dataset_loader)
+        pipeline_name = self.get_pipeline_name(
+            transformer, dataset_loader, n_transforms_orig)
+        pipeline_save_file_name = '%s_step%i.pkl' % (
+            pipeline_name, len(self.pipeline_transformation_selectors) - 1)
+        final_save_path = os.path.join(self.results_folder_path,
+                                       pipeline_save_file_name)
+        return final_save_path
 
     def get_selected_transformer(self,
         transformer: AbstractTransformer, x_data: np.array,
-        dataset_loader: HiTSOutlierLoader):
+        dataset_loader: HiTSOutlierLoader, load_from_checkpoint=True):
+        n_transforms_orig = transformer.n_transforms
+        final_save_path = self._get_pipeline_final_save_path(
+            transformer, n_transforms_orig, dataset_loader)
+        if os.path.exists(final_save_path) and load_from_checkpoint:
+            selected_transformations = pd.read_pickle(final_save_path)
+            transformer.set_transformations_to_perform(selected_transformations)
+            print('n selected transforms %i' % (transformer.n_transforms))
+            return transformer
         self.print_manager.verbose_printing(self.verbose_pipeline)
         for step_i, selector in enumerate(
             self.pipeline_transformation_selectors):
             print('Transform Selection %s' % selector.name)
             transformer = selector.get_selected_transformer(
                 transformer, x_data, dataset_loader)
-            self._save_selected_transformations(step_i, transformer,
-                                                dataset_loader)
             print('n selected transforms %i' % (transformer.n_transforms))
+        self._save_selected_transformations(
+            step_i, transformer, n_transforms_orig, dataset_loader)
         self.print_manager.close()
         return transformer
 
 
 if __name__ == '__main__':
     VERBOSE_PIPELINE = True
-    VERBOSE_SELECTORS = False
+    VERBOSE_SELECTORS = True
     from parameters import loader_keys, general_keys
     from modules.geometric_transform.transformer_for_ranking import \
         RankingTransformer
@@ -118,7 +144,7 @@ if __name__ == '__main__':
     }
     ztf_loader = ZTFSmallOutlierLoader(ztf_params)
 
-    data_loader = ztf_loader  # hits_loader  #
+    data_loader = hits_loader  #ztf_loader  #
 
     (x_train, y_train), (
         x_val, y_val), _ = data_loader.get_outlier_detection_datasets()
