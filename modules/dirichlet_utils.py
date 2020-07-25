911b95a345cdffba5d8d0b7197759276472d978f
ReyesDeJong <reyes.dejong@gmail.com>
2020-07-25 18:04:09 -0400

    bug is somewhere aroun calc_approx_alpha_sum

--- modules/dirichlet_utils.py
+++ modules/dirichlet_utils.py
@@ -9,12 +9,28 @@ sys.path.append(PROJECT_PATH)
 
 from scipy.special import psi, polygamma
 from modules.utils import normalize_sum1
+from modules.utils import check_path
 
 # ToDo: see if this can be done by tf
 
 def calc_approx_alpha_sum(observations):
   N = len(observations)
   f = np.mean(observations, axis=0)
+
+  result_alpha = (N * (len(f) - 1) * (-psi(1))) / (
+      N * np.sum(f * np.log(f)) - np.sum(
+      f * np.sum(np.log(observations), axis=0)))
+
+  if result_alpha < 0:
+    log_file_folder = os.path.join(PROJECT_PATH, 'results', '1a_error_log')
+    check_path(log_file_folder)
+    log_file_path = os.path.join(log_file_folder, 'calc_approx_alpha_sum.log')
+    log_file = open(log_file_path, 'w')
+    log_file.write(str(N) + '\n')
+    log_file.write(str(f) + '\n')
+    log_file.close()
+    np.save(observations, os.path.join(log_file_folder, 'observation.npy'))
+  assert result_alpha<0
 
   return (N * (len(f) - 1) * (-psi(1))) / (
       N * np.sum(f * np.log(f)) - np.sum(
@@ -52,17 +68,16 @@ def fixed_point_dirichlet_mle(alpha_init, log_p_hat, max_iter=1000):
     # if alpha_old_sum%1==0 and alpha_old_sum<0:
     #   alpha_old_sum=+0.5
     alpha_new = inv_psi(psi(alpha_old_sum) + log_p_hat)
-    if not np.isfinite(alpha_new).all() or np.sum(alpha_new)<0:
-      log_file_path = os.path.join(PROJECT_PATH, 'results', '1a_error.log')
-      log_file = open(log_file_path, 'w')
-      log_file.write(str(alpha_init)+'\n')
-      log_file.write(str(alpha_old)+'\n')
-      log_file.write(str(log_p_hat)+'\n')
-      log_file.write(str(alpha_old_sum)+'\n')
-      log_file.write(str(psi(alpha_old_sum))+'\n')
-      log_file.write(str(psi(alpha_old_sum) + log_p_hat)+'\n')
-      log_file.close()
-      assert np.isfinite(alpha_new).all() or np.sum(alpha_new)<0
+    # if not np.isfinite(alpha_new).all() or np.sum(alpha_new)<0:
+    #   log_file_path = os.path.join(PROJECT_PATH, 'results', '1a_error.log')
+    #   log_file = open(log_file_path, 'w')
+    #   log_file.write(str(alpha_init)+'\n')
+    #   log_file.write(str(alpha_old)+'\n')
+    #   log_file.write(str(log_p_hat)+'\n')
+    #   log_file.write(str(alpha_old_sum)+'\n')
+    #   log_file.write(str(psi(alpha_old_sum))+'\n')
+    #   log_file.write(str(psi(alpha_old_sum) + log_p_hat)+'\n')
+    #   log_file.close()
     if np.sqrt(np.sum((alpha_old - alpha_new) ** 2)) < 1e-9:
       break
     # if not np.isfinite(alpha_new).all():
@@ -87,15 +102,14 @@ def dirichlet_score(predict_x_train, predict_x_eval):
   alpha_sum_approx = calc_approx_alpha_sum(
       observed_dirichlet)
   alpha_0 = observed_dirichlet.mean(axis=0) * alpha_sum_approx
-  if np.sum(alpha_0) < 0:
-    log_file_path = os.path.join(PROJECT_PATH, 'results', '1a_error_dirichlet_score.log')
-    log_file = open(log_file_path, 'w')
-    log_file.write(str(alpha_0) + '\n')
-    log_file.write(str(alpha_sum_approx) + '\n')
-    log_file.write(str(observed_dirichlet.mean(axis=0)) + '\n')
-    log_file.write(str(observed_dirichlet) + '\n')
-    log_file.close()
-    assert np.sum(alpha_0) < 0
+  # if np.sum(alpha_0) < 0:
+  #   log_file_path = os.path.join(PROJECT_PATH, 'results', '1a_error_dirichlet_score.log')
+  #   log_file = open(log_file_path, 'w')
+  #   log_file.write(str(alpha_0) + '\n')
+  #   log_file.write(str(alpha_sum_approx) + '\n')
+  #   log_file.write(str(observed_dirichlet.mean(axis=0)) + '\n')
+  #   log_file.write(str(observed_dirichlet) + '\n')
+  #   log_file.close()
   mle_alpha_t = fixed_point_dirichlet_mle(alpha_0, log_p_hat_train)
   diri_score = dirichlet_normality_score(mle_alpha_t, x_eval_p)
   return diri_score
---
+++ script_correctness_checkers/dirichlet/testeing_apha_calc.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.special import psi
+
+def calc_approx_alpha_sum(observations):
+  N = len(observations)
+  f = np.mean(observations, axis=0)
+
+  return (N * (len(f) - 1) * (-psi(1))) / (
+      N * np.sum(f * np.log(f)) - np.sum(
+      f * np.sum(np.log(observations), axis=0)))
+
+def normalize_sum1(array, axis=-1):
+  sums = np.sum(array, axis=axis)
+  return array / np.expand_dims(sums, axis)
+
+n_a = 100
+n_pred = 9
+a = np.random.random((n_a,n_pred))
+random_ints = np.random.randint(0, high=n_pred, size=n_a)
+# random_ints = np.array(list(zip(np.arange(n_a),random_ints)))
+# a[random_ints] = 10
+a[np.arange(n_a), random_ints] = 0
+# print(a)
+a[a==0] = 1e-10
+a_norm = normalize_sum1(a)
+# print(a_norm)
+# print(np.sum(a_norm,axis=-1))
+alpha = calc_approx_alpha_sum(a_norm)
+print(alpha)
