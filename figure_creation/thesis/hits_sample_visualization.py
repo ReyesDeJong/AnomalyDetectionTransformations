a5f9707811ad70d52027af002b015aa4a3ab2cd9
ReyesDeJong <reyes.dejong@gmail.com>
2020-08-31 22:14:10 -0400

    trying to visualize hits

---
+++ figure_creation/thesis/hits_sample_visualization.py
@@ -0,0 +1,38 @@
+"""
+Visualization of HiTS sample for paper, 4 samples per class
+"""
+
+import os
+import sys
+
+PROJECT_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(PROJECT_PATH)
+
+
+from parameters import loader_keys, general_keys, param_keys
+from modules.data_loaders.hits_outlier_loader import HiTSOutlierLoader
+
+
+
+
+
+if __name__ == '__main__':
+    # data loaders
+    hits_params = {
+        loader_keys.DATA_PATH: os.path.join(
+            PROJECT_PATH, '../datasets/HiTS2013_300k_samples.pkl'),
+        loader_keys.N_SAMPLES_BY_CLASS: 150000,
+        loader_keys.TEST_PERCENTAGE: 0.2,
+        loader_keys.VAL_SET_INLIER_PERCENTAGE: 0.1,
+        loader_keys.USED_CHANNELS: [0, 1, 2],
+        loader_keys.CROP_SIZE: 21,
+        general_keys.RANDOM_SEED: 42,
+        loader_keys.TRANSFORMATION_INLIER_CLASS_VALUE: 1
+    }
+    hits_loader = HiTSOutlierLoader(hits_params)
+    hits_loader.set_pickle_loading(False)
+    hits_loader.set_pickle_saving(False)
+
+    hits_loader.get
+
